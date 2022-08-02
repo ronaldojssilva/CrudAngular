e76c72a913f1960df11f27899dbe14dc51974660
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-01 22:25:12 -0400

    Add Decorator Input

--- src/app/salary-color.directive.ts
+++ src/app/salary-color.directive.ts
@@ -1,12 +1,25 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
-  selector: '[SalaryColor]' //camelCase
+  selector: '[salaryColor]' //camelCase
 })
 export class SalaryColorDirective {
 
+  @Input()
+  salaryColor: string = '';
+
+  @Input()
+  x: any;
+
   constructor(private element: ElementRef) {
-    this.element.nativeElement.innerHTML = 'teste';
+    setTimeout(()=> {
+      const nativeElement: HTMLElement = this.element.nativeElement;
+      const salary = parseFloat(this.salaryColor);
+      nativeElement.style.color = salary > 20000 ? 'green' : 'red';
+      console.log(this.x)
+      // this.element.nativeElement.innerHTML = this.salaryColor;
+    }, 2000)
+
   }
 
 }
