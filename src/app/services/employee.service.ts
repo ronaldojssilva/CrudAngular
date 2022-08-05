99a1a43c3ea0fb6b48d2d8f190dcbfc795740ef3
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-05 15:51:12 -0400

    ngSwitch

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -15,6 +15,10 @@ export class EmployeeService {
     {name:'Fulano de Tal', salary: 1000, bonus: 0},
     {name:'Cicrano da Silva', salary: 10000, bonus: 0},
     {name:'Beltrano de Tal', salary: 900, bonus: 5},
+    {name:'Funcionario X', salary: 800, bonus: 10},
+    {name:'Funcionario Y', salary: 700, bonus: 100},
+    {name:'Funcionario Z', salary: 600, bonus: 1000},
+    {name:'Funcionario A', salary: 600, bonus: 10},
   ];
 
   constructor() { }
